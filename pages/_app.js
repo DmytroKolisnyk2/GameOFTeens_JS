21ac6bfb99446d33f9d7c07e6dbc49b3840fff45
Max Popsuy <maxim09p@gmail.com>
2022-07-01 22:15:50 +0300

    polar Area and result page added

--- pages/_app.js
+++ pages/_app.js
@@ -1,51 +1,45 @@
-import Head from 'next/head';
-import "../styles/reset.scss"
-import '../styles/global.scss';
+import Head from "next/head";
+import "../styles/reset.scss";
+import "../styles/global.scss";
+import Diagram from "../components/Diagram/Diagram.jsx";
 
 import { Provider } from "react-redux";
-import {
-  store,
-  persistor
-} from "../redux/store";
+import { store, persistor } from "../redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-import NotificationContainer from 'react-notifications/lib/NotificationContainer';
+import NotificationContainer from "react-notifications/lib/NotificationContainer";
 import ScrollTopArrow from "../components/ScrollTopArrow/ScrollTopArrow";
 import NextNProgress from "nextjs-progressbar";
 
-
 function MyApp({ Component, pageProps }) {
-  return <>
-    <Head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#000000" />
-      <meta name="description" content="description" />
-      <link
-        rel="icon shortcut"
-        href="../logo.jpg"
-        type="image/x-icon"
-      />
-      {Component.title ? <title>{Component.title}</title> : <title>Project</title>}
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="description" />
+        <link rel="icon shortcut" href="../logo.jpg" type="image/x-icon" />
+        {Component.title ? (
+          <title>{Component.title}</title>
+        ) : (
+          <title>Project</title>
+        )}
+      </Head>
 
-    </Head>
+      <div id="root-modal"></div>
+      <PersistGate loading={null} persistor={persistor}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </PersistGate>
 
-    <div id="root-modal"></div>
+      <NextNProgress color="tomato" showOnShallow={true} />
 
-    <PersistGate loading={null} persistor={persistor}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </PersistGate>
-
-    <NextNProgress
-      color="tomato"
-      showOnShallow={true}
-    />
-
-    <ScrollTopArrow bgColor="red" />
-    <NotificationContainer />
-  </>
+      <ScrollTopArrow bgColor="red" />
+      <NotificationContainer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
